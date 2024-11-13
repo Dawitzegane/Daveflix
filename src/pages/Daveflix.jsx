134b8a3e7dd4602e6ee5f4692da1c620fb0ceaff
dawit zegane <dawitzegane@gmail.com>
2024-11-13 21:15:08 +0300

    edited navbar a litle bit

--- src/pages/Daveflix.jsx
+++ src/pages/Daveflix.jsx
@@ -4,6 +4,7 @@ import backgroundImage from "../assets/home.jpg";
 import MovieLogo from "../assets/homeTitle.webp";
 import { FaPlay } from "react-icons/fa";
 import { AiOutlineInfoCircle } from "react-icons/ai";
+import styled from "styled-components";
 export default function Daveflix() {
   const [isScrolled, setIsScrolled] = useState(false);
   window.onscroll = () => {
@@ -12,7 +13,7 @@ export default function Daveflix() {
   };
 
   return (
-    <div>
+    <Container>
       <Navbar isScrolled={isScrolled} />
       <div className="hero">
         <img
@@ -34,6 +35,55 @@ export default function Daveflix() {
           </div>
         </div>
       </div>
-    </div>
+    </Container>
   );
 }
+const Container = styled.div`
+  background-color: black;
+  .hero {
+    position: relative;
+    .background-image {
+      filter: brightness(60%);
+    }
+    img {
+      height: 100vh;
+      width: 100vw;
+    }
+    .container {
+      position: absolute;
+      bottom: 5rem;
+      .logo {
+        img {
+          width: 100%;
+          height: 100%;
+          margin-left: 5rem;
+        }
+      }
+      .buttons {
+        margin: 5rem;
+        gap: 2rem;
+        button {
+          font-size: 1.4rem;
+          gap: 1rem;
+          border-radius: 0.2rem;
+          padding: 0.5rem;
+          padding-left: 2rem;
+          padding-right: 2.4rem;
+          border: none;
+          cursor: pointer;
+          transition: 0.3s ease-in-out;
+          &:hover {
+            opacity: 0.8;
+          }
+          &:nth-of-type(2) {
+            background-color: rgba(109, 109, 110, 0.7);
+            color: white;
+            svg {
+              font-size: 1.8rem;
+            }
+          }
+        }
+      }
+    }
+  }
+`;
